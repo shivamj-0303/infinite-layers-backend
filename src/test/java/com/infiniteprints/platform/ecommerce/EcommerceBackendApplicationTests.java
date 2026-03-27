00d23552f43d6154551482c9ced62ac32691f8d3
shivamj0303 <jangidshivam2002@gmail.com>
2026-03-27 12:21:02 +0530

    Feat: Initialise spring

---
+++ src/test/java/com/infiniteprints/platform/ecommerce/EcommerceBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infiniteprints.platform.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
